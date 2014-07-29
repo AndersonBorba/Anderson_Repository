d4f654e9542b3e6602d7c3c73ef12d85dad25f43
AndersonBorba <anderson_borba@outlook.com>
2014-07-29 17:25:47 -0300

    Teste

---
+++ Tea/Logica/ImprimeNome.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tea
+{
+    public class ImprimeNome
+    {
+        public void imprimeNome()
+        {
+            for (int i = 1; i <= 100; ++i)
+            {
+                Console.WriteLine("Anderson");   
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ Tea/Logica/Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tea.Logica
+{
+    public class Menu
+    {
+         static void Main(string[] args)
+        {
+            ImprimeNome nome = new ImprimeNome();
+
+            Console.WriteLine("========MENU=======");
+            Console.WriteLine("1- Padrão 1");
+            Console.WriteLine("2- Padrão 2");
+            Console.WriteLine("3- Padrão 3");
+            Console.WriteLine("4- Padrão 4");
+            int i = int.Parse(Console.ReadLine());
+
+
+
+            if (i == 1)
+            {
+                nome.imprimeNome();
+            }
+
+            Console.ReadKey();
+                
+        }
+    }
+}
