9141b651fbbc3b91362a9a1103f0eb7acbb216b8
AndersonBorba <anderson_borba@outlook.com>
2014-07-30 11:24:33 -0300

    Exercício Pronto

--- Tea/Logica/ImprimeNome.cs
+++ Tea/Logica/ImprimeNome.cs
@@ -14,7 +14,7 @@ namespace Tea
             {
                 Console.WriteLine("Anderson");   
             }
-            Console.ReadKey();
+         
         }
         
     }
--- Tea/Logica/Main.cs
+++ Tea/Logica/Main.cs
@@ -8,26 +8,78 @@ namespace Tea.Logica
 {
     public class Menu
     {
-         static void Main(string[] args)
+        static void Main(string[] args)
         {
             ImprimeNome nome = new ImprimeNome();
+            Numeros numero = new Numeros();
+            Piramide piramide = new Piramide();
+            int i = 20;
 
-            Console.WriteLine("========MENU=======");
-            Console.WriteLine("1- Padrão 1");
-            Console.WriteLine("2- Padrão 2");
-            Console.WriteLine("3- Padrão 3");
-            Console.WriteLine("4- Padrão 4");
-            int i = int.Parse(Console.ReadLine());
-
-
-
-            if (i == 1)
+            try
             {
-                nome.imprimeNome();
-            }
+                while (i != 0)
+                {
+                    Console.WriteLine("================================================================================");
+                    Console.WriteLine("                MENU                               ");
+                    Console.WriteLine(" 1 - Imprime Nome                                 1");
+                    Console.WriteLine(" 2 - Imprime Numeros de um a cem                  2");
+                    Console.WriteLine(" 3 - Imprime 1 asterisco para impar e 2 para par  3");
+                    Console.WriteLine(" 4 - Imprime multiplos de                         4");
+                    Console.WriteLine(" 5 - Imprime uma piramide                         5");
+                    Console.WriteLine(" 6 - Imprime varias piramides                     6");
+                    Console.WriteLine(" 7 - Serie de Fibonacci                           7");
+                    Console.WriteLine(" Pressione 0 para parar ");
+                    Console.WriteLine("================================================================================");
 
-            Console.ReadKey();
-                
+                    i = int.Parse(Console.ReadLine());
+
+
+                    if (i <= 7 && i >= 0)
+                    {
+                        if (i == 1)
+
+                            nome.imprimeNome();
+
+                        if (i == 2)
+
+                            numero.imprimeNumeroDe1a100();
+
+                        if (i == 3)
+
+                            numero.imprimeAsterisco1ParaImpar2ParaPar();
+
+                        if (i == 4)
+
+                            numero.imprimePIParaMultipolsDeQuatro();
+
+                        if (i == 5)
+
+                            piramide.imprimeUmaPiramide();
+
+                        if (i == 6)
+
+                            piramide.imprimeVariasPiramides();
+
+                        if (i == 7)
+
+                            numero.serieDeFibonacci();
+
+                    }
+
+                    else
+
+                        Console.WriteLine("Numero invalido");
+
+                }
+            }
+            catch 
+            {
+
+                Console.WriteLine("Um caractere desconhecido foi inserido ou o enter foi pressionado no lugar de um numero");
+                Console.ReadKey();
+            }
+           
+
         }
     }
 }
