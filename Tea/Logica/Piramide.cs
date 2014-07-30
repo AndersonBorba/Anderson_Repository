9b669415cc6ff2c51219c3fb4bf3796b9e867f9f
AndersonBorba <anderson_borba@outlook.com>
2014-07-30 11:30:21 -0300

    Exercicio Pronto

---
+++ Tea/Logica/Piramide.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tea.Logica
+{
+    class Piramide
+    {
+        public void imprimeUmaPiramide()
+        {
+            Console.WriteLine("  *");
+            Console.WriteLine(" * *");
+            Console.WriteLine("* * *");
+
+        }
+
+        public void imprimeVariasPiramides()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine("  *  ");
+                Console.WriteLine(" * * ");
+                Console.WriteLine("* * *");
+                Console.WriteLine("            ");
+            }
+         
+            
+        }
+
+    }
+}
